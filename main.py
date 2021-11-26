277307239c36aed9da289c56efed004283c0066b
quarbby <lynnette-ng@outlook.com>
2021-11-26 09:31:20 -0500

    started

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import plotly.express
+
+st.set_page_config(page_title="Voter Fraud", page_icon=None, layout='centered', initial_sidebar_state='expanded', menu_items=None)
+
+st.title('Voter Fraud')
+
